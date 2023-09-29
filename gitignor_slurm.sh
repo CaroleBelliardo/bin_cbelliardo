c7748cb762f9955ef84e9a03bd8af6161512a0f5
Carole Belliardo <belliardo.carole@hotmail.fr>
2023-09-29 19:02:33 +0200

    gitignor

--- gitignor_slurm.sh
+++ gitignor_slurm.sh
@@ -1,28 +1,22 @@
 #!/bin/bash
 
-# Fonction pour parcourir les répertoires récursivement
-update_gitignore() {
-  local dir="$1"
-  local gitignore_file="$dir/.gitignore"
+rep="$1"
 
-  if [ -f "$gitignore_file" ]; then
-    # Vérifier si .gitignore contient déjà des règles pour .err et .out
-    if grep -qE '(\.err|\.out)$' "$gitignore_file"; then
-      echo "Les règles pour .err et .out existent déjà dans $dir/.gitignore"
-    else
-      echo -e "\n# Ignorer les fichiers .err et .out\n*.err\n*.out" >> "$gitignore_file"
-      echo "Règles ajoutées à $dir/.gitignore"
-    fi
-  fi
+if [ -z "$rep" ]; then
+  echo "Usage: $0 <répertoire>"
+  exit 1
+fi
 
-  # Parcourir les sous-répertoires
-  for subdir in "$dir"/*/; do
-    if [ -d "$subdir" ]; then
-      update_gitignore "$subdir"
-    fi
-  done
-}
+gitignore_file="$rep/.gitignore"
 
-# Appel de la fonction avec le répertoire courant
-update_gitignore "$(pwd)"
+
+# Ajouter les règles pour ignorer les fichiers .err et .out s'ils n'y sont pas déjà
+if ! grep -qE '(\.err|\.out)$' "$gitignore_file "; then
+  echo "# Ignorer les fichiers .err et .out" > "$gitignore_file"
+  echo "*.err" >> "$gitignore_file"
+  echo "*.out" >> "$gitignore_file"
+  echo "Les fichiers .err et .out seront ignorés dans le suivi Git de ce répertoire."
+else
+  echo "Les règles pour ignorer les fichiers .err et .out existent déjà dans le fichier .gitignore."
+fi
 
